7751bac66a05959ea4ab12349e05d6b862a60760
imtoshaa <imtoshaa.work@gmail.com>
2022-03-14 18:48:55 +0300

    fix db

--- src/main/java/com/tms/App.java
+++ src/main/java/com/tms/App.java
@@ -6,7 +6,6 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-// Аннотация, которая объединяет в себя @Configuration, @EnableAutoConfiguration, @ComponentScan
 @SpringBootApplication
 public class App {
     public static void main(String[] args) throws TelegramApiException {
